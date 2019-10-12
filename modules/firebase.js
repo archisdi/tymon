4d5a81ce3374cfe7b647b701c45bd32bcf586fa2
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-10-12 17:59:14 +0700

    update firebase

--- modules/firebase.js
+++ modules/firebase.js
@@ -5,25 +5,29 @@ const path = require("path");
 let instance;
 const initialize = (input) => {
     if (!instance) {
-        const filePath = path.join(__dirname, '../../..', input.service_account_path);
-        const serviceAccount = require(filePath);
-        const params = {
-            type: serviceAccount.type,
-            projectId: serviceAccount.project_id,
-            privateKeyId: serviceAccount.private_key_id,
-            privateKey: serviceAccount.private_key,
-            clientEmail: serviceAccount.client_email,
-            clientId: serviceAccount.client_id,
-            authUri: serviceAccount.auth_uri,
-            tokenUri: serviceAccount.token_uri,
-            authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-            clientC509CertUrl: serviceAccount.client_x509_cert_url
-        };
-        instance = firebase.initializeApp({
-            credential: firebase.credential.cert(params),
+        const config = {
             databaseURL: input.db_url,
             storageBucket: input.storage_url
-        });
+        };
+        // load cred if passed
+        if (input.service_account_path) {
+            const filePath = path.join(__dirname, '../../..', input.service_account_path);
+            const serviceAccount = require(filePath);
+            const params = {
+                type: serviceAccount.type,
+                projectId: serviceAccount.project_id,
+                privateKeyId: serviceAccount.private_key_id,
+                privateKey: serviceAccount.private_key,
+                clientEmail: serviceAccount.client_email,
+                clientId: serviceAccount.client_id,
+                authUri: serviceAccount.auth_uri,
+                tokenUri: serviceAccount.token_uri,
+                authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
+                clientC509CertUrl: serviceAccount.client_x509_cert_url
+            };
+            config.credential = firebase.credential.cert(params);
+        }
+        instance = firebase.initializeApp(config);
     }
 };
 const getInstance = () => {
--- modules/firebase.ts
+++ modules/firebase.ts
@@ -1,37 +1,49 @@
 import * as firebase from 'firebase-admin';
 import * as path from 'path';
 
+interface IInstanceConfig {
+  databaseURL?: string;
+  storageBucket?: string;
+  credential?: any;
+}
+
 interface IFirebaseInput {
-    db_url?: string;
-    storage_url?: string;
-    service_account_path: string;
+  db_url?: string;
+  storage_url?: string;
+  service_account_path: string;
 }
 
 let instance: any;
 
 const initialize = (input: IFirebaseInput): void => {
   if (!instance) {
-      const filePath: string = path.join(__dirname, '../../..', input.service_account_path);
-      const serviceAccount = require(filePath);
-
-      const params = {
-        type: ( serviceAccount as any).type,
-        projectId: ( serviceAccount as any).project_id,
-        privateKeyId: ( serviceAccount as any).private_key_id,
-        privateKey: ( serviceAccount as any).private_key,
-        clientEmail: ( serviceAccount as any).client_email,
-        clientId: ( serviceAccount as any).client_id,
-        authUri: ( serviceAccount as any).auth_uri,
-        tokenUri: ( serviceAccount as any).token_uri,
-        authProviderX509CertUrl: ( serviceAccount as any).auth_provider_x509_cert_url,
-        clientC509CertUrl: ( serviceAccount as any).client_x509_cert_url
-      };
-
-      instance = firebase.initializeApp({
-        credential: firebase.credential.cert(params),
+      const config: IInstanceConfig = {
         databaseURL: input.db_url,
         storageBucket: input.storage_url
-      });
+      };
+
+      // load cred if passed
+      if (input.service_account_path) {
+        const filePath: string = path.join(__dirname, '../../..', input.service_account_path);
+        const serviceAccount = require(filePath);
+
+        const params = {
+          type: ( serviceAccount as any).type,
+          projectId: ( serviceAccount as any).project_id,
+          privateKeyId: ( serviceAccount as any).private_key_id,
+          privateKey: ( serviceAccount as any).private_key,
+          clientEmail: ( serviceAccount as any).client_email,
+          clientId: ( serviceAccount as any).client_id,
+          authUri: ( serviceAccount as any).auth_uri,
+          tokenUri: ( serviceAccount as any).token_uri,
+          authProviderX509CertUrl: ( serviceAccount as any).auth_provider_x509_cert_url,
+          clientC509CertUrl: ( serviceAccount as any).client_x509_cert_url
+        };
+
+        config.credential = firebase.credential.cert(params);
+      }
+
+      instance = firebase.initializeApp(config);
   }
 };
 
